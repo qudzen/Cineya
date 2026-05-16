45058ee9e43fc803282b088793ccde02d52d6ce1
vvelt <pavlovvasa860@gmail.com>
2026-05-16 17:15:51 +1000

    Меню + маршрутизация

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import logo from './assets/cineya.png';
+import {Routes, Route, NavLink} from "react-router-dom";
+import Movies from "./Pages/Movies.tsx";
+import Home from "./Pages/Home.tsx";
 
 function App() {
 
-  return (
-    <div></div>
-  )
+    return (
+        <>
+            <header>
+                <img src={logo} alt='логотип сайта' className='lg:w-15'/>
+                <NavLink to="/">Главная</NavLink>
+                <NavLink to="/movies">Фильмы</NavLink>
+            </header>
+          <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/movies' element={<Movies />} />
+          </Routes>
+        </>
+    )
 }
 
 export default App
---
+++ src/Pages/Movies.tsx
@@ -0,0 +1,5 @@
+export default function Movies(){
+    return (
+        <div>12131311231</div>
+    )
+}
