b1eaaf99190f2c97e52c3496f9f0f291a80d4d71
vvelt <pavlovvasa860@gmail.com>
2026-05-17 15:58:59 +1000

    Слайдер фильмов

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -13,6 +13,7 @@ interface Result {
 
 export default function Home() {
     const [popularFilmResult, setPopularFilmResult] = useState<Result[]>([])
+    const [indexSlider, setIndexSlider] = useState<number>(0)
 
     useEffect(() => {
         const popularFilm = async () => {
@@ -23,11 +24,42 @@ export default function Home() {
         popularFilm()
     }, [])
 
+    const nextFilm = () => {
+        if (indexSlider === 4){
+            setIndexSlider(0)
+        }
+        else {
+            setIndexSlider(prev => prev + 1)
+        }
+    }
+
+    const prevFilm = () => {
+        if (indexSlider === 0){
+            setIndexSlider(4)
+        }
+        else {
+            setIndexSlider(prev => prev - 1)
+        }
+    }
+
+    const sliderFilms = popularFilmResult.slice(0, 5)
+    const otherFilms = popularFilmResult.slice(5)
+
     return (
         <>
+            {sliderFilms.length > 0 && (
+                <div className='flex gap-2'>
+                        <div className='border-2 border-black'>
+                            <img src={`https://image.tmdb.org/t/p/original${sliderFilms[indexSlider].backdrop_path}`} alt=""/>
+                            {sliderFilms[indexSlider].title}
+                        </div>
+                    <button onClick={nextFilm}> Следующий </button>
+                    <button onClick={prevFilm}> Предидущий </button>
+                </div>
+            )}
             {popularFilmResult.length > 0 && (
                 <div className='flex gap-2'>
-                    {popularFilmResult.map(film => (
+                    {otherFilms.map(film => (
                         <div className='border-2 border-black'>
                             <img src={`https://image.tmdb.org/t/p/original${film.poster_path}`} alt=""/>
                             {film.title}
